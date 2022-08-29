3d17822eba204a4efd145718ade15c5f3fdd60c6
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-08-29 19:17:59 +0100

    fixed GET {{baseUrl}}/sensors/1/ request

--- measurement/views.py
+++ measurement/views.py
@@ -1,8 +1,4 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateAPIView
 from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
 
